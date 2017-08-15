321dbaf3e0c1905a7d8fff18c8615654efa4cd10
John Mitsch <jomitsch@redhat.com>
2017-08-15 17:06:43 -0400

    changed homepage text

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Katmobile</h2>
+          <h3>The future is here</h3>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, point your phone at your system and yell loudly
         </p>
       </div>
     );
